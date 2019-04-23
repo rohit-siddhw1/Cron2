f83f62160b92343642979c0e4354c2535a04245c
rohit-siddhw1 <rohit.siddheshwar@gmail.com>
2019-04-23 18:47:19 +0530

    init

---
+++ CronClass.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rohitsid
+ * Date: 23/4/19
+ * Time: 6:16 PM
+ */
+class CronClass
+{
+    public function execute() {
+        return "Testing";
+    }
+}
---
+++ cron.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rohitsid
+ * Date: 23/4/19
+ * Time: 6:15 PM
+ */
+
+include_once "CronClass.php";
+
+$cronClassObj = new CronClass();
+echo $cronClassObj->execute();
